e31f032c67ca7b19dc9fa6d3dc3d0fcf0a5a62b8
thrcorrea <thales.rcorrea@gmail.com>
2019-04-07 17:39:56 -0300

    Adicionado dockerfiles para o bankService, database e docker compose

---
+++ mysql/sql-scripts/CreateTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `banks` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(3) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
---
+++ mysql/sql-scripts/InsertData.sql
@@ -0,0 +1,3 @@
+INSERT INTO banking.banks
+(id, code, description)
+VALUES(1, '033', 'Santander');
